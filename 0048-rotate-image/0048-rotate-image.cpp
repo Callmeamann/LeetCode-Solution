e10da57b1782d633ba147bcd1e43dd84c06aeda3
Aman Gusain <124049374+Callmeamann@users.noreply.github.com>
2023-09-03 12:32:11 +0530

    Time: 4 ms (36.93%), Space: 7 MB (54.71%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    void transpose(vector<vector<int>> &matrix , int n){
+        for(int i=0 ; i<n ; i++){
+            for(int j=i+1 ; j<n ; j++){
+                if(i!=j)
+                {
+                    swap(matrix[i][j] , matrix[j][i]);
+                }
+            }
+        }
+    }
+
+
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        transpose(matrix,n);
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<n/2 ; j++){
+                swap(matrix[i][j],matrix[i][n-j-1]);
+            }
+        }
+    }
+};
