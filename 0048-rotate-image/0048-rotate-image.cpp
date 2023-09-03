bca2817ab04a6fb1ccc8045c67c7d02526aeadfc
Aman Gusain <124049374+Callmeamann@users.noreply.github.com>
2023-09-03 12:34:54 +0530

    Time: 4 ms (36.93%), Space: 7.3 MB (18.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -15,13 +15,13 @@ private:
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-
+        reverse(matrix.begin(),matrix.end());
         transpose(matrix,n);
 
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<n/2 ; j++){
-                swap(matrix[i][j],matrix[i][n-j-1]);
-            }
-        }
+        // for(int i=0 ; i<n ; i++){
+        //     for(int j=0 ; j<n/2 ; j++){
+        //         swap(matrix[i][j],matrix[i][n-j-1]);
+        //     }
+        // }
     }
 };
