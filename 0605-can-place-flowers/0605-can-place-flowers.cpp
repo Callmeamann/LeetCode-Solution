f87373ffa0b15583590e1caf38c9d61729159035
Aman Gusain <124049374+Callmeamann@users.noreply.github.com>
2023-09-04 21:23:43 +0530

    Time: 11 ms (85.17%), Space: 20.4 MB (28.33%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,16 +4,18 @@ public:
 
         int sz = flowerbed.size();
         int count = 0;
-        if(sz == 1 and n == 1 ){
-            if(flowerbed[0] == 0)
-                return true;
-            else 
-                return false;
-        }
+        // if(sz == 1 and n == 1 ){
+        //     if(flowerbed[0] == 0)
+        //         return true;
+        //     else 
+        //         return false;
+        // }
             
         for(int i=0 ; i<sz ; i++){
             if(flowerbed[i] == 0){
                 if(i==0 ){ 
+                    if(i==sz-1)
+                        count++;
                     if( i+1 < sz and flowerbed[i+1] == 0) {
                         count++;
                         flowerbed[i] = 1;
