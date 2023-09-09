524e190a42bd0284de1c1aa07fa74955fde65c4a
Aman Gusain <124049374+Callmeamann@users.noreply.github.com>
2023-09-09 12:52:04 +0530

    Time: 0 ms (100%), Space: 6.7 MB (46.24%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    // Dynamic Programming Top - down approach
     unordered_map<int,int> umap;
     int solve(vector<int>& nums,int remain){
         if(remain == 0)
@@ -19,6 +20,21 @@ private:
 
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        return solve(nums,target);
+        // solve(nums,target);
+        sort(nums.begin(), nums.end());
+        vector<double> dp(target + 1, 0.0); // Use double for signed integer overflow
+
+        dp[0] = 1.0;
+
+        for (int csum = 1; csum <= target; csum++) {
+            for (int n : nums) {
+                if (csum - n >= 0) {
+                    dp[csum] += dp[csum - n];
+                } else {
+                    break;
+                }
+            }
+        }
+        return static_cast<int>(dp[target]);
     }
 };
