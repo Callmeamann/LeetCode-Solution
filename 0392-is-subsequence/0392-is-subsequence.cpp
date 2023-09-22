71af68eeee022ba3e38dbc9d8918b8589486d8fa
Aman Gusain <124049374+Callmeamann@users.noreply.github.com>
2023-09-22 16:01:20 +0530

    Time: 0 ms (100%), Space: 6.6 MB (17.6%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int si = 0, sn = s.size(),tn = t.size();
+        if(sn == 0)
+            return true;
         if(sn > tn)
             return false;
         for(int i=0 ; i< tn; ++i){
             if(s[si] == t[i]){
                 si++;
-            }
-            if(sn == si)
-                return true;
+                if(sn == si)
+                    return true;
+            }   
         }
-        if(si == sn)
-            return true;
         return false;
     }
 };
