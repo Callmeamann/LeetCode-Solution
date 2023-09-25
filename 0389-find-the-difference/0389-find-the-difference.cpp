93d3eaf664702122218d7fcfaf4eec157ba01ab6
Aman Gusain <124049374+Callmeamann@users.noreply.github.com>
2023-09-25 20:31:12 +0530

    Time: 0 ms (100%), Space: 7 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<vector<int>> freq(26,vector<int>(2,0));
-        for(int i=0 ; i<t.size() ; ++i){
-            if(i<s.size())
-                freq[s[i]-'a'][0]++;
-            freq[t[i]-'a'][1]++;
+        for(int i=0 ; i<s.size() ; ++i){
+            t[i+1] += t[i]-s[i];
         }
-        char ch = 'a';
-        for(int i=0 ; i<26 ; ++i){
-            if(freq[i][0] != freq[i][1]){
-                ch = 'a'+i;
-                break;                
-            }
-        }
-        return ch;
+        return t[t.size()-1];
     }
 };
