82fcc014021317515ae9f8804c07322478e66938
Aman Gusain <124049374+Callmeamann@users.noreply.github.com>
2023-10-01 13:15:40 +0530

    Time: 10 ms (92.55%), Space: 10.2 MB (50.32%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    void revWord(string &str, int l,int r){
+      while(l<r){
+        swap(str[l++],str[r--]);
+      }
+    }
+public:
+    string reverseWords(string s) {
+        int i=0 ,n=s.length();
+        int ti=0;
+        
+        while(i<n){
+          if(s[i] == ' '){
+            revWord(s,ti,i-1);
+            i++;
+            ti = i;
+            continue;
+          }
+            i++;
+        }
+        revWord(s,ti,n-1);
+        return s;
+
+    }
+};
